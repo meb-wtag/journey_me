314650ea306e21e30aec578c19386c02e9e42cdf
meb-wtag <melvin.eberle@welldev.io>
2023-08-31 15:16:36 +0200

    ..

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,33 @@
+class JournalsController < ApplicationController
+
+	def index
+		@journals = Journal.all
+	end
+
+	def new
+		@journal = Journal.new
+	end
+
+	def create
+		@journal = Journal.new(journal_params)
+		if @journal.save
+			redirect_to journals_path 
+		end
+	end
+
+	def destroy
+		@journal = Journal.find(params[:id])
+		@journal.destroy
+
+		redirect_to journals_path
+	end
+
+	def show
+    @journal = Journal.find(params[:id])
+  end
+
+	private
+	def journal_params
+		params.require(:journal).permit(:title, :description)
+	end
+end
