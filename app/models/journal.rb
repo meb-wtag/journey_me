68b6c1eef6e9e2f621ea0db821c3c00418adffe7
meb-wtag <melvin.eberle@welldev.io>
2023-08-31 15:19:05 +0200

    Resolved spacing problem
    Tabs are converted into spaces

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,6 +1,6 @@
 class Journal < ApplicationRecord
 
-	validates :title, presence: true
-	validates :description, presence: true
-	
+  validates :title, presence: true
+  validates :description, presence: true
+  
 end
