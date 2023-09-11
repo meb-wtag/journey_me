5bf2950e706d40c81d56f1520c980d4aa365b494
meb-wtag <melvin.eberle@welldev.io>
2023-09-11 13:49:02 +0200

    implementing i18n

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 RSpec.describe UsersController, type: :model do 
   let(:user) do
-    FactoryBot.build(:user, first_name = nil)
+    FactoryBot.build(:user, first_name: nil)
   end
 	it 'is not valid without a first_name' do
     expect(user.valid?).to be_falsey
   end
 
   let(:user2) do
-    FactoryBot.build(:user, last_name = nil)
+    FactoryBot.build(:user, last_name: nil)
   end
   it 'is not valid without a last_name' do
     expect(user2.valid?).to be_falsey
   end 
 end
+
