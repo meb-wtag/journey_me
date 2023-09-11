1692fb59efa256f36deb0b3daac917f6c413a117
meb-wtag <melvin.eberle@welldev.io>
2023-09-11 13:56:49 +0200

    Implemented i18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,29 +12,38 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = t('user.message.success.create')
+      redirect_to user_path(@user) 
+    else
+      flash[:error] = t('user.message.error.create')
       redirect_to user_path(@user) 
     end
   end
 
-  def edit
-    @user = User.find(params[:id]) 
-  end
-
   def update
     @user = User.find(params[:id]) 
-    @user = User.update(user_params)
-    redirect_to user_path(@user)
+    if User.update(user_params)
+      flash[:success] = t('user.message.success.update')
+      redirect_to user_path(@user)
+    else
+      flash[:error] = t('user.message.error.update')
+      redirect_to user_path(@user)
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-
-    redirect_to new_user_path
+    if @user.destroy
+      flash[:success] = t('user.message.success.delete')
+      redirect_to new_user_path
+    else
+      flash[:error] = t('user.message.error.delete')
+      redirect_to new_user_path
   end
 
   def show
-    @user = User.find(params[:id])
+    if User.find(params[:id])
+    else
+      flash[:error] = t('user.message.error.show')
   end
 
   private
