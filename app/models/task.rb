a4e70e214b7a60929703ca2407fbd729a28ed267
meb-wtag <melvin.eberle@welldev.io>
2023-09-25 17:01:59 +0200

    s

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  after_initialize :set_default_importance, if: :new_record?
+  after_initialize :set_default_status, if: :new_record?
+
+  validates :title, presence: true
+  validates :description, presence: true
+
+  enum importance: %i[unimportant medium important]
+  enum status: %i[todo progress done]
+
+  def set_default_importance
+    self.importance ||= :user
+  end
+
+  def set_default_status
+    self.status ||= :user
+  end
 end
--- db/migrate/20230925145442_create_tasks.rb
+++ db/migrate/20230925145442_create_tasks.rb
@@ -2,7 +2,10 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.integer :user_id, null: false, index: true, foreign_key: true
-      t.text :description, null: false
+      t.string :title, null: false
+      t.content :description, null: false
+      t.date :deadline,
+
 
       t.timestamps
     end
