46f2783b46b333f5821b33b2c4607195da3f39ef
meb-wtag <melvin.eberle@welldev.io>
2023-09-26 13:50:24 +0200

    Edited Migrations again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_103841) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_085555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,11 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_103841) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "java_scripts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "journal_entries", force: :cascade do |t|
@@ -81,7 +76,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_103841) do
     t.date "joining_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
   end
