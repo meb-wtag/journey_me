dd6390605ba859d7be496687ac2997afad54ce51
meb-wtag <melvin.eberle@welldev.io>
2023-09-27 10:17:39 +0200

    requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,6 @@ class UsersController < ApplicationController
   before_action :require_login, except: %i[index new create upload_profile_picture]
   load_and_authorize_resource
 
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,14 @@ class Ability
     if user.present?
       can %i(create read update destroy), Journal
       can %i(create read update destroy), JournalEntry
-      can %i(create show new ), User
-      can :all, UserSession
-
+      can %i(create show new), User
+      can :update, User, id: user.id
+      cannot :index, User
       if user.admin?
         can :manage, :all
       end
+    else
+      can %i(create show new), User
     end
   end
 end
