8dbfff37cc4cb31e49603b529db1dddb788d819d
meb-wtag <melvin.eberle@welldev.io>
2023-09-27 09:46:33 +0200

    fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,11 @@ class Ability
 
   def initialize(user)
     if user.present?
-      can %i(create read update destroy), Journal
-      can %i(create read update destroy), JournalEntry
-      can %i(create show new ), User
-      can :all, UserSession
-
-      if user.admin?
-        can :manage, :all
-      end
+      can :manage, :all
+      # can %i(create read update destroy), Journal
+      # can %i(create read update destroy), JournalEntry
+      # can %i(create show new ), User
+      # can :all, UserSession
     end
   end
 end
