6ee1342ea6b15af268fffcef675851b9b34f2b48
meb-wtag <melvin.eberle@welldev.io>
2023-09-27 13:19:03 +0200

    exeption message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :authenticate_user
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: "#{exception.message}"
+  end
+
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,13 +3,13 @@ class Ability
 
   def initialize(user)
     if user.present?
-      can %i(create read update destroy), Journal
-      can %i(create read update destroy), JournalEntry
-      can %i(create show new), User
-      can :update, User, id: user.id
-      cannot :index, User
       if user.admin?
         can :manage, :all
+      else
+        can %i(create read update destroy), [Journal, JournalEntry]
+        can %i(create show new), User
+        can :update, User, id: user.id
+        cannot :index, User
       end
     else
       can %i(create show new), User
