1ce3ef6debb49313011f5183b1e21359923c4cd1
meb-wtag <melvin.eberle@welldev.io>
2023-10-02 15:28:09 +0200

    Fixxed Specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
-  validates :password, presence: true
+  validates :role, presence: true
   validates :password_confirmation, presence: true, on: :create
   validates :profile_picture,
             blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 0..(5.megabytes) }
 
-  enum role: %i[user admin]
+  enum role: { user: 0, admin: 1 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,32 @@
 require 'rails_helper'
-RSpec.describe UsersController, type: :model do
-  let(:user1) do
-    FactoryBot.build(:user, username: nil)
+RSpec.describe User, type: :model do
+  let(:user) do
+    FactoryBot.build(:user)
   end
-  it 'is not valid without a first_name' do
-    expect(user1.valid?).to be_falsey
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:role) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
   end
 
   let(:user2) do
-    FactoryBot.build(:user, password: nil)
-  end
-  it 'is not valid without a password' do
-    expect(user2.valid?).to be_falsey
-  end
-
-  let(:user3) do
-    FactoryBot.build(:user, password_confirmation: nil)
-  end
-  it 'is not valid without a password_confirmation' do
-    expect(user3.valid?).to be_falsey
-  end
-
-  let(:user4) do
     FactoryBot.build(:user, password: '12', password_confirmation: '13')
   end
   it 'is not valid when password and password_confirmation not identical' do
-    expect(user4.valid?).to be_falsey
+    expect(user2.valid?).to be_falsey
   end
 
   let!(:user5) do
     FactoryBot.create(:user, username: 'test')
   end
   let(:user6) do
-    FactoryBot.build(:user, username: 'test')
+    FactoryBot.create(:user, username: 'test')
   end
-  it 'is not valid when naming 2 users with same username' do
-    expect(user6.save).to be_falsey
-  end
+  it { is_expected.to validate_uniqueness_of(:username) }
 
-  let(:user7) do
-    FactoryBot.build(:user)
-  end
-  it 'when creating a user it has the role of a user' do
-    expect(user7.role).to eq 'user'
+  describe 'associations' do
+    it { is_expected.to have_many(:journals) }
   end
 end
