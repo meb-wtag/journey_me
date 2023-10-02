f40f33b715413f0608f52c4f90745fa21e0228b2
meb-wtag <melvin.eberle@welldev.io>
2023-10-02 14:53:23 +0200

    Requested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,13 @@ class User < ApplicationRecord
   has_many :journals, dependent: :destroy, inverse_of: :user
   has_one_attached :profile_picture
   has_secure_password :password, validations: true
-  after_initialize :set_default_role, if: :new_record?
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
+  validates :password, presence: true
   validates :password_confirmation, presence: true, on: :create
   validates :profile_picture,
             blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 0..(5.megabytes) }
 
   enum role: %i[user admin]
-
-  def set_default_role
-    self.role ||= :user
-  end
 end
