5e76dabcbdd3d4534dd8fdb886f115072bf084f4
meb-wtag <melvin.eberle@welldev.io>
2023-10-02 14:28:11 +0200

    Corrected Ability Spec

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,40 +5,34 @@ RSpec.describe Ability do
   subject(:ability) { Ability.new(user) }
 
   describe 'for admin users' do
-    let(:user) { FactoryBot.create(:user, role: 'admin') }
+    let(:user) { FactoryBot.create(:user, role: :admin) }
 
     it { should be_able_to(:manage, :all) }
   end
 
   describe 'for regular users' do
-    let(:user) { FactoryBot.create(:user, role: 'user') }
+    let(:user) { FactoryBot.create(:user, role: :user) }
 
-    it { should be_able_to(:create, Journal) }
-    it { should be_able_to(:read, Journal) }
-    it { should be_able_to(:update, Journal) }
-    it { should be_able_to(:destroy, Journal) }
+    it { is_expected.to be_able_to(:create, Journal) }
+    it { is_expected.to be_able_to(:read, Journal) }
+    it { is_expected.to be_able_to(:update, Journal) }
+    it { is_expected.to be_able_to(:destroy, Journal) }
 
-    it { should be_able_to(:create, JournalEntry) }
-    it { should be_able_to(:read, JournalEntry) }
-    it { should be_able_to(:update, JournalEntry) }
-    it { should be_able_to(:destroy, JournalEntry) }
+    it { is_expected.to be_able_to(:create, JournalEntry) }
+    it { is_expected.to be_able_to(:read, JournalEntry) }
+    it { is_expected.to be_able_to(:update, JournalEntry) }
+    it { is_expected.to be_able_to(:destroy, JournalEntry) }
 
-    it { should be_able_to(:create, User) }
-    it { should be_able_to(:show, User) }
-    it { should be_able_to(:new, User) }
-    it { should be_able_to(:update, User, id: user.id) }
-    it { should_not be_able_to(:index, User) }
+    it { is_expected.to be_able_to(:create, User) }
+    it { is_expected.to be_able_to(:show, User) }
+    it { is_expected.to be_able_to(:new, User) }
+    it { is_expected.to be_able_to(:update, User, id: user.id) }
   end
 
   describe 'for guests' do
     let(:user) { nil }
 
-    it { should be_able_to(:create, User) }
-    it { should be_able_to(:new, User) }
-    it { should_not be_able_to(:create, Journal) }
-    it { should_not be_able_to(:create, JournalEntry) }
-    it { should_not be_able_to(:show, User) }
-    it { should_not be_able_to(:update, User) }
-    it { should_not be_able_to(:index, User) }
+    it { is_expected.to be_able_to(:create, User) }
+    it { is_expected.to be_able_to(:new, User) }
   end
 end
