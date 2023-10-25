d3839574cb8abf5b8991571d60f20073e2ba5561
meb-wtag <melvin.eberle@welldev.io>
2023-10-25 13:21:07 +0200

    Search on hover

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
 	def index
-		@query = Journal.ransack(params[:q])
+		@journalQuery = Journal.ransack(params[:q])
 		@journals = @journalQuery.result(distinct: true)
+		@entryQuery = JournalEntry.ransack(params[:q])
+		@entries = @entryQuery.result(distinct: true)
 	end
 end
