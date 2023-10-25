8130b28f434064764a68b50d6ab61809ba5bfce0
meb-wtag <melvin.eberle@welldev.io>
2023-10-25 13:46:38 +0200

    restructure css

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,5 @@ class SearchController < ApplicationController
 	def index
 		@journalQuery = Journal.ransack(params[:q])
 		@journals = @journalQuery.result(distinct: true)
-		@entryQuery = JournalEntry.ransack(params[:q])
-		@entries = @entryQuery.result(distinct: true)
 	end
 end
