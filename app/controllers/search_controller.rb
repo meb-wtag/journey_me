19af0169eaa79d1f1ef615c67e11cdebf5c3800d
meb-wtag <melvin.eberle@welldev.io>
2023-10-25 14:05:38 +0200

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 	def index
-		@journalQuery = Journal.ransack(params[:q])
+		@journalQuery = current_user.journals.ransack(params[:q])
 		@journals = @journalQuery.result(distinct: true)
 	end
 end
